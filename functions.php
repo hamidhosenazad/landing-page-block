7a4bf05221ecc59f3d7016b1d7b84c65852d91f0
hamidhosenazad <69019197+hamidhosenazad@users.noreply.github.com>
2023-04-13 00:45:12 +0600

    Initial commit

---
+++ functions.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Functions and definitions
+ *
+ * @package landing-page-block
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @since 1.0.0
+ */
+
+/**
+* Enqueue the style files.
+*
+* @since 1.0.0
+*
+* @return mixed
+*/
+function landing_page_block_styles()
+{
+    wp_enqueue_style(
+        'landing-page-block-style',
+        get_stylesheet_uri(),
+        '',
+        '1.0.0'
+    );
+	wp_enqueue_style( 'fontawesome', get_stylesheet_directory_uri() . '/assets/css/font_awesome/all.css', array(), 'all' );
+	
+}	
+add_action('wp_enqueue_scripts', 'landing_page_block_styles');
+
+/**
+ * Register block styles
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function landing_page_block_register_block_styles() {
+
+	register_block_style( // phpcs:ignore WPThemeReview.PluginTerritory.ForbiddenFunctions.editor_blocks_register_block_style
+		'core/button',
+		array(
+			'name'  => 'landing-page-block-crimson-flat-button',
+			'label' => __( 'Flat crimson button', 'landing-page-block' ),
+		)
+	);
+
+	register_block_style( // phpcs:ignore WPThemeReview.PluginTerritory.ForbiddenFunctions.editor_blocks_register_block_style
+		'core/button',
+		array(
+			'name'  => 'landing-page-block-crimson-round-button',
+			'label' => __( 'Round crimson button', 'landing-page-block' ),
+		)
+	);
+
+	register_block_style( // phpcs:ignore WPThemeReview.PluginTerritory.ForbiddenFunctions.editor_blocks_register_block_style
+		'core/button',
+		array(
+			'name'  => 'landing-page-block-white-flat-button',
+			'label' => __( 'Flat white button', 'landing-page-block' ),
+		)
+	);
+}
+add_action( 'init', 'landing_page_block_register_block_styles' );
+
+/**
+ * Registers block patterns.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function landing_page_block_patterns() {
+
+	// Array of block pattern names.
+	$block_patterns = array(
+		'list-menu'
+	);
+
+	foreach ( $block_patterns as $block_pattern ) {
+		$pattern_file = get_theme_file_path( 'block-patterns/' . $block_pattern . '.php' );
+		register_block_pattern(
+			'landing-page-block/' . $block_pattern,
+			require $pattern_file
+		);
+	}
+}
+add_action( 'init', 'landing_page_block_patterns', 9 );
+
+/**
+ * Add theme support for block styles.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function landing_page_block_setup()
+{
+    add_theme_support('wp-block-styles');
+    add_editor_style('style.css');
+}
+add_action('after_setup_theme', 'landing_page_block_setup');
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This file has been left empty on purpose.
+ *
+ * @package landing-page-block
+ *
+ * @link https://core.trac.wordpress.org/ticket/54272
+ *
+ * @since 1.0.0
+ */
